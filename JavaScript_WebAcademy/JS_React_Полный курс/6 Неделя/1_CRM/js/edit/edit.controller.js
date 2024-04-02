b43259edb2386ac02d46fcc666a7b747ff11b53a
Vachik_Tovmasyan <tovm2504@mail.ru>
2024-04-02 12:57:57 +0300

    WebAcademy. 6 неделя. CRM

--- JavaScript_WebAcademy/JS_React_Полный курс/6 Неделя/1_CRM/js/edit/edit.controller.js
+++ JavaScript_WebAcademy/JS_React_Полный курс/6 Неделя/1_CRM/js/edit/edit.controller.js
@@ -13,48 +13,13 @@ function getRequstId() {
   const resultRequst = req.filter((item) => item.id === idReq);
   console.log("Вроде это", resultRequst);
 
-  function renderResultRequst(itemSearch) {
-    const idHTML = `<div class="col">Заявка №<span id="number"> ${itemSearch[0].id}</span></div>`;
-    editView.elementsTable.numberID.insertAdjacentHTML("beforebegin", idHTML);
-
-    const dateHTML = `<div class="col" id="date">${itemSearch[0].date}</div> `;
-    editView.elementsTable.date.insertAdjacentHTML("afterend", dateHTML);
-
-
-    function rendSelectOptionsProduct() {
-      const as = editView.elementsTable.product.getElementsByTagName("option");
-      for (let i = 0; i < as.length; i++) {
-        if (as[i].value === itemSearch[0].product) {
-          as[i].selected = itemSearch[0].product;
-        }
-      }
-    }
-    rendSelectOptionsProduct();
-    
-
-    const nameHTML = `<input type="text" class="form-control" value="${itemSearch[0].name}" id="name" name="name"/>`;
-    editView.elementsTable.name.insertAdjacentHTML("afterbegin", nameHTML);
-                          
-    const emailHTML = `<input type="text" class="form-control" value="${itemSearch[0].email}" id="email" name="email"/>`;
-    editView.elementsTable.email.insertAdjacentHTML("afterbegin", emailHTML);
-  
-    const phoneHTML = `<input type="text" class="form-control" value="${itemSearch[0].phone}" id="phone" name="phone"/>`;
-    editView.elementsTable.phone.insertAdjacentHTML("afterbegin", phoneHTML);
-  
-
-    
-    function rendSelectOptionsStatus() {
-      const as = editView.elementsTable.status.getElementsByTagName("option");
-      for (let i = 0; i < as.length; i++) {
-        if (as[i].value === itemSearch[0].status) {
-          as[i].selected = itemSearch[0].status;
-        }
-      }
-    }
-    rendSelectOptionsStatus();
-
-  }
-
-  renderResultRequst(resultRequst);
+  editView.renderResultRequst(resultRequst);
 }
 init();
+
+
+
+editView.elementsTable.saveEdit.addEventListener("click", function (e) {
+  e.preventDefault()
+
+})
--- JavaScript_WebAcademy/JS_React_Полный курс/6 Неделя/1_CRM/js/edit/edit.view.js
+++ JavaScript_WebAcademy/JS_React_Полный курс/6 Неделя/1_CRM/js/edit/edit.view.js
@@ -6,6 +6,51 @@ const elementsTable = {
   email: document.getElementById("emailHTML"),
   phone: document.getElementById("phoneHTML"),
   status: document.getElementById("status"),
-};
+  saveEdit: document.getElementById("saveEdit"),
+  col: document.querySelectorAll(".col"),
+  coll: document.querySelectorAll('[data-attr="collArr"]'),
 
-export { elementsTable };
+};
+const qw = Array.from(elementsTable.coll)
+// console.log(qw)
+
+qw.forEach(el => console.log(el))
+
+function renderResultRequst(itemSearch) {
+  const idHTML = `<div class="col">Заявка №<span id="number"> ${itemSearch[0].id}</span></div>`;
+  elementsTable.numberID.insertAdjacentHTML("beforebegin", idHTML);
+
+  const dateHTML = `<div class="col" id="date">${itemSearch[0].date}</div> `;
+  elementsTable.date.insertAdjacentHTML("afterend", dateHTML);
+
+  function rendSelectOptionsProduct() {
+    const as = elementsTable.product.getElementsByTagName("option");
+    for (let i = 0; i < as.length; i++) {
+      if (as[i].value === itemSearch[0].product) {
+        as[i].selected = itemSearch[0].product;
+      }
+    }
+  }
+  rendSelectOptionsProduct();
+
+  const nameHTML = `<input type="text" class="form-control" value="${itemSearch[0].name}" id="name" name="name"/>`;
+  elementsTable.name.insertAdjacentHTML("afterbegin", nameHTML);
+
+  const emailHTML = `<input type="text" class="form-control" value="${itemSearch[0].email}" id="email" name="email"/>`;
+  elementsTable.email.insertAdjacentHTML("afterbegin", emailHTML);
+
+  const phoneHTML = `<input type="text" class="form-control" value="${itemSearch[0].phone}" id="phone" name="phone"/>`;
+  elementsTable.phone.insertAdjacentHTML("afterbegin", phoneHTML);
+
+  function rendSelectOptionsStatus() {
+    const as = elementsTable.status.getElementsByTagName("option");
+    for (let i = 0; i < as.length; i++) {
+      if (as[i].value === itemSearch[0].status) {
+        as[i].selected = itemSearch[0].status;
+      }
+    }
+  }
+  rendSelectOptionsStatus();
+}
+
+export { elementsTable, renderResultRequst };
