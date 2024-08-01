0af29ed33471b17856437c152512731d9b620885
Vachik_Tovmasyan <tovm2504@mail.ru>
2024-08-02 00:42:00 +0300

    GoTrip

--- HTML_CSS_Верстка/3. Trip/js/main.js
+++ HTML_CSS_Верстка/3. Trip/js/main.js
@@ -1,9 +1,16 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    center: true,
-    startPosition: 1,
-    loop: true,
-    margin: 30,
-    item:3
-  });
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+  center: true,
+  startPosition: 1,
+  loop: true,
+  margin: 30,
+  item:3
 });
+
+$('.slader_btn_prev').click(function() {
+  owl.trigger('prev.owl.carousel');
+})
+
+$('.slader_btn_next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
